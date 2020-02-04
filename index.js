c2f2233468c29930fde684ec8e830c98964dc301
Andrew Dalmedo <andrewdalmedo@gmail.com>
2020-02-04 03:34:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,3 +26,13 @@ function prependKitten(name) {
   var newArray = [name, ...kittens];
   return newArray;
 }
+
+function removeLastKitten() {
+  var newArray = kittens.slice(0, kittens.length - 1);
+  return newArray;
+}
+
+function removeFirstKitten() {
+  var newArray = kittens.slice(1);
+  return newArray;
+}
